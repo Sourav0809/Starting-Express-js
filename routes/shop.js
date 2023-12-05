33d96bb56e2b6e85a359243596313914d9641c76
Sourav0809 <spathak7431@gmail.com>
2023-12-05 22:03:15 +0530

    DONE

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,19 @@ const express = require('express')
 const path = require('path')
 const router = express.Router()
 
-
+const rootDir = require('../util/path')
 router.get('/', (req, res, next) => {
     console.log("IN THE HOME PAGE MIDDLEWARE ")
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 
 
+})
+
+
+
+router.get('/contact', (req, res, next) => {
+    console.log("in the about page ")
+    res.sendFile(path.join(rootDir, "views", 'contact.html'))
 })
 
 
