999d8ac451c8fd82b7ef2699288ea67c2525af5a
Sourav0809 <spathak7431@gmail.com>
2023-12-05 21:39:47 +0530

    done

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const rootDir = require('../util/path')
+
 // /admin/add-product => GET 
+// Create an Express app
+const app = express();
 
 router.get('/add-product', (req, res, next) => {
     console.log("IN THE ADD PRODUCT MIDDLEWARE")
-    res.sendFile(path.join(__dirname, "../", "views", 'add-product.html'))
+    res.sendFile(path.join(rootDir, "views", 'add-product.html'))
 })
 
 
